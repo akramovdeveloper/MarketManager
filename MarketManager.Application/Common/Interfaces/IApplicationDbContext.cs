8126ecd32fd8be82f1be08a05b4803e37e0d5b3c
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-17 19:57:13 +0500

    Architechture builds

---
+++ MarketManager.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MarketManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketManager.Application.Common.Interfaces;
+public interface IApplicationDbContext
+{
+
+    public DbSet<User> Users { get; }
+
+}
