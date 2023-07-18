9000684597df408ff3a0097c2ed3c8f1f29bfbfe
Zafarbek <=>
2023-07-18 16:46:34 +0500

    PaymentType added

---
+++ MarketManager.Application/UseCases/PaymentTypes/Commands/DeletePaymentType/DeletePaymentTypeCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManager.Application.UseCases.PaymentTypes.Commands.DeletePaymentType
+{
+    internal class DeletePaymentTypeCommand
+    {
+    }
+}
