1daf949497f5eaecb825e4eb89a4f1b0705a91e7
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-18 11:58:14 +0500

    fixed expired products

--- MarketManager.Application/UseCases/ExpiredProducts/Queries/GetByIdExpiredProducts/GetByIdExpiredProductsQuery.cs
+++ MarketManager.Application/UseCases/ExpiredProducts/Queries/GetByIdExpiredProducts/GetByIdExpiredProductsQuery.cs
@@ -1,10 +1,31 @@
-﻿using MarketManager.Application.Common.DTOs.ExpiredProductDtos;
+﻿using AutoMapper;
+using MarketManager.Application.Common.Interfaces;
+using MarketManager.Application.Common.Models;
+using MarketManager.Domain.Entities;
 using MediatR;
 
 namespace MarketManager.Application.UseCases.ExpiredProducts.Queries
 {
     public class GetByIdExpiredProductsQuery : IRequest<ExpiredProductGetDto>
     {
+        public Guid Id { get; set; }
+    }
 
+    public class GetByIdExpiredProductsQueryHandler : IRequestHandler<GetByIdExpiredProductsQuery, ExpiredProductGetDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetByIdExpiredProductsQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<ExpiredProductGetDto> Handle(GetByIdExpiredProductsQuery request, CancellationToken cancellationToken)
+        {
+            ExpiredProduct? res = _context.ExpiredProducts.FirstOrDefault(x=>x.Id== request.Id);
+            if()
+        }
     }
 }
--- MarketManager.Domain/Entities/ExpiredProduct.cs
+++ MarketManager.Domain/Entities/ExpiredProduct.cs
@@ -1,13 +1,11 @@
 ﻿namespace MarketManager.Domain.Entities
 {
-    public class ExpiredProduct
+    public class ExpiredProduct : BaseAuditableEntity
     {
-        public Guid Id { get; set; }
+       
         public Guid PackageId { get; set; }
         public virtual Package Packages { get; set; }
         public int Count { get; set; }
         public DateTime DeletedTime { get; set; }
-        public Guid UserId { get; set; }
-        public virtual User Users { get; set; }
     }
 }
