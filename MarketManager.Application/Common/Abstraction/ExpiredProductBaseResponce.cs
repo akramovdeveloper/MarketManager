432465d053ae7c9635565f280ad6aaaa858cd7ce
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-18 18:19:13 +0500

    commit stage

---
+++ MarketManager.Application/Common/Abstraction/ExpiredProductBaseResponce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManager.Application.Common.Abstraction
+{
+    public abstract class ExpiredProductBaseResponce
+    {
+        public Guid PackageId { get; set; }
+        public int Count { get; set; }
+        public DateTime DeletedTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public Guid CreatedById { get; set; }
+        public Guid ModifyById { get; set; }
+    }
+}
---
+++ MarketManager.Application/UseCases/ExpiredProducts/Command/CreateExpiredProduct/CreateExpiredProductCommand.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MarketManager.Application.Common.Abstraction;
+using MarketManager.Application.Common.Interfaces;
+using MarketManager.Domain.Entities;
+using MediatR;
+
+namespace MarketManager.Application.UseCases.ExpiredProducts.Command.CreateExpiredProduct
+{
+    public class CreateExpiredProductCommand : IRequest<CreateExpiredProductResponce>
+    {
+        public Guid PackageId { get; set; }
+        public int Count { get; set; }
+        
+    }
+    public class CreateExpiredProductResponce : ExpiredProductBaseResponce
+    {
+
+    }
+
+    public class CreateExpiredProductCommandHandler : IRequestHandler<CreateExpiredProductCommand, CreateExpiredProductResponce>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        
+        public CreateExpiredProductCommandHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public Task<CreateExpiredProductResponce> Handle(CreateExpiredProductCommand request, CancellationToken cancellationToken)
+        {
+            var expiredProduct = new ExpiredProduct
+            {
+                Id = Guid.NewGuid(),
+                PackageId = request.PackageId,
+                Count = request.Count,
+                DeletedTime = DateTime.Now,
+                CreatedDate = DateTime.Now
+            };
+        }
+    }
+
+}
