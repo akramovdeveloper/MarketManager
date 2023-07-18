f6a023d90070e359e78b3d7a819b6e28b816d355
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-18 08:53:50 +0500

    save

---
+++ MarketManager.Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MarketManager.Application.Common.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                string requestName = typeof(TRequest).Name;
+                Log.Error(ex, $"Market Manager Request: Unhandled Exception for request {requestName} {request}\n");
+                throw;
+            }
+        }
+    }
+}
