7b554eab9a950fa61470df1d0d58f647688de0e9
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-18 08:59:28 +0500

    save

--- MarketManager.Application/Common/Models/ExpiredProductGetDto.cs
+++ MarketManager.Application/Common/Models/ExpiredProductGetDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MarketManager.Application.Common.DTOs.ExpiredProductDtos
+namespace MarketManager.Application.Common.Models
 {
     internal class ExpiredProductGetDto
     {
