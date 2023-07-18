f5ce1f04f4052ebf817849b6f74ad4f88d342a69
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-07-18 23:47:25 +0300

    bla bla

--- MarketManager.Application/UseCases/Roles/Commands/DeleteRole/DeleteRoleCommand.cs
+++ MarketManager.Application/UseCases/Roles/Commands/DeleteRole/DeleteRoleCommand.cs
@@ -1,4 +1,24 @@
-﻿namespace MarketManager.Application.UseCases.Roles.Commands.DeleteRole;
-public class DeleteRoleCommand
+﻿using MarketManager.Application.Common.Interfaces;
+using MediatR;
+
+namespace MarketManager.Application.UseCases.Roles.Commands.DeleteRole;
+public record DeleteRoleCommand : IRequest
 {
+    public Guid Id { get; set; }
+}
+
+public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteRoleCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Roles.FindAsync(new object[] { request.Id });
+
+    }
 }
