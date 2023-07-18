1e258e39b14b46a6e7e514fc7303dacb91867553
Dilshod <dilshodsh97@gmail.com>
2023-07-18 13:40:13 +0500

    update models

---
+++ MarketManager.API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using MarketManager.Application.Common.Models;
+using MarketManager.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ResponseCore<List<Product>>>> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
