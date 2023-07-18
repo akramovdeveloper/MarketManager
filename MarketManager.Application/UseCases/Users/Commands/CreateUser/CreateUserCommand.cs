cbf0e7e5631dc86255d27946ecc63d61bd340c1a
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-18 18:59:25 +0500

    fixed minor bugs

--- MarketManager.Application/UseCases/Users/Commands/CreateUser/CreateUserCommand.cs
+++ MarketManager.Application/UseCases/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -3,12 +3,17 @@
 namespace MarketManager.Application.UseCases.Users.Commands.CreateUser;
 public class CreateUserCommand:IRequest<Guid>
 {
+    public string FullName { get; set; }
+    public string Phone { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 
 }
 public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Guid>
 {
     public Task<Guid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
+
         throw new NotImplementedException();
     }
 }
