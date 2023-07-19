185d1235845adf779fe30ba8e70bb1a4797d6efe
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-19 08:42:34 +0500

    save

---
+++ MarketManager.Application/UseCases/Products/Queries/GetByIdProduct/GetByIdProductQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketManager.Application.UseCases.Products.Queries.GetByIdProduct
+{
+    internal class GetByIdProduct
+    {
+    }
+}
