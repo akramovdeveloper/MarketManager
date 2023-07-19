ce80e1e4bc2151bae9b8bd08402e5779c41df10f
Javlonbek <122851751+Javlonbek2106@users.noreply.github.com>
2023-07-19 15:53:29 +0500

    save

---
+++ MarketManager.Application/Common/Mappings/CartMapping.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MarketManager.Application.UseCases.Carts.Commands.CreateCart;
+using MarketManager.Application.UseCases.Carts.Commands.DeleteCart;
+using MarketManager.Application.UseCases.Carts.Commands.UpdateCart;
+using MarketManager.Domain.Entities;
+
+namespace MarketManager.Application.Common.Mappings
+{
+    public class CartMapping : Profile
+    {
+        public CartMapping()
+        {
+            CartMappings();
+            CartWithCart();
+        }
+
+        private void CartWithCart()
+        {
+            CreateMap<CreateCartCommand, Cart>();
+            CreateMap<UpdateCartCommand, Cart>();
+            CreateMap<DeleteCartCommand, Cart>();
+        }
+
+        private void CartMappings()
+        {
+            CreateMap<Cart, GetCartByIdResponse>();
+            CreateMap<Cart, GetAllCartQueryResponse>();
+        }
+    }
+}
