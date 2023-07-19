e6bbc804afb5b658973c34e7f66c4849f8d636cf
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-19 17:37:46 +0500

    Crud changes of User

--- MarketManager.Application/UseCases/Users/Commands/RegisterUser/RegisterUserCommand.cs
+++ MarketManager.Application/UseCases/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -1,4 +1,38 @@
-﻿namespace MarketManager.Application.UseCases.Users.Commands.RegisterUser;
-public class RegisterUserCommand
+﻿using AutoMapper;
+using MarketManager.Application.Common.Interfaces;
+using MarketManager.Application.Common.JWT.Interfaces;
+using MarketManager.Application.Common.JWT.Models;
+using MarketManager.Domain.Entities.Identity;
+using MediatR;
+
+namespace MarketManager.Application.UseCases.Users.Commands.RegisterUser;
+public class RegisterUserCommand:IRequest<TokenResponse>
 {
+    public string FullName { get; set; }
+    public string Username { get; set;} 
+    public string Phone { get; set; }
+    public string Password { get; set;}
+    public string ConfirmPassword { get; set;}
+}
+public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, TokenResponse>
+{
+    private readonly IJwtToken _jwtToken;
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+
+    public RegisterUserCommandHandler(IJwtToken jwtToken, IApplicationDbContext context, IMapper mapper)
+    {
+        _jwtToken = jwtToken;
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<TokenResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = _mapper.Map<User>(request); 
+        await _context.Users.AddAsync(user,cancellationToken);
+        await  _context.SaveChangesAsync(cancellationToken);
+        var tokenResponse = await _jwtToken.CreateTokenAsync(user.Username, user.Roles, cancellationToken);
+        return tokenResponse;
+    }
 }
--- MarketManager.Application/UseCases/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
+++ MarketManager.Application/UseCases/Users/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace MarketManager.Application.UseCases.Users.Commands.RegisterUser;
-public class RegisterUserCommandValidator
+public class RegisterUserCommandValidator:AbstractValidator<RegisterUserCommand>    
 {
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(user => user.FullName).NotEmpty().WithMessage("Full name is required.");
+        RuleFor(user => user.Username).NotEmpty().WithMessage("Username is required.");
+        RuleFor(user => user.Phone).NotEmpty().WithMessage("Phone number is required.");
+        RuleFor(user => user.Password).NotEmpty().WithMessage("Password is required.");
+        RuleFor(user => user.ConfirmPassword)
+            .Equal(user => user.Password).WithMessage("Passwords do not match.");
+    }
 }
