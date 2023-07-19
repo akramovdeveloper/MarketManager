294d002a0c2db5286778c27fba0a8bbb9a231d7d
Dilshod <dilshodsh97@gmail.com>
2023-07-19 15:43:26 +0500

    Incoming date renamed

--- MarketManager.Application/UseCases/Packages/Commands/CreatePackage/CreatePackageCommand.cs
+++ MarketManager.Application/UseCases/Packages/Commands/CreatePackage/CreatePackageCommand.cs
@@ -13,7 +13,7 @@ namespace MarketManager.Application.UseCases.Packages.Commands.CreatePackage
         public double IncomingPrice { get; set; }
         public double SalePrice { get; set; }
 
-        public DateTime IncomingDate { get; set; }
+        public DateTime Date { get; set; }
     }
     public class CreatePackageCommandHandler : IRequestHandler<CreatePackageCommand, Guid>
     {
