505368825867c11d3966205ae5aabbf56637e7ec
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-07-19 09:20:56 +0500

    fixed userId

--- MarketManager.Domain/Commons/BaseAuditableEntity.cs
+++ MarketManager.Domain/Commons/BaseAuditableEntity.cs
@@ -5,9 +5,9 @@ namespace MarketManager.Domain.Entities;
 public class BaseAuditableEntity : BaseEntity
 {
     public DateTime CreatedDate { get; set; }
-    public DateTime ModifyDate { get; set; }
-    public Guid CreatedById { get; set; }
-    public virtual User CreatedBy { get; set; }
-    public Guid ModifyById { get; set; }
-    public virtual User ModifyBy { get; set; }
+    public DateTime? ModifyDate { get; set; }
+    public Guid? CreatedById { get; set; }
+    public virtual User? CreatedBy { get; set; }
+    public Guid? ModifyById { get; set; }
+    public virtual User? ModifyBy { get; set; }
 }
